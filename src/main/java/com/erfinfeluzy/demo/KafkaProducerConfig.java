5045c2243a897aa0a78f228409a36aeb6ebce644
erfin feluzy <erfin.feluzy@erfinfeluzy.com>
2020-04-02 02:58:06 +0700

    Update KafkaProducerConfig.java

--- src/main/java/com/erfinfeluzy/demo/KafkaProducerConfig.java
+++ src/main/java/com/erfinfeluzy/demo/KafkaProducerConfig.java
@@ -15,15 +15,15 @@ import org.springframework.kafka.core.ProducerFactory;
 public class KafkaProducerConfig {
 
 	@Bean
-	   public ProducerFactory<String, String> producerFactory() {
-	      Map<String, Object> configProps = new HashMap<>();
-	      configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-	      configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-	      configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-	      return new DefaultKafkaProducerFactory<>(configProps);
-	   }
-	   @Bean
-	   public KafkaTemplate<String, String> kafkaTemplate() {
-	      return new KafkaTemplate<>(producerFactory());
-	   }
+	public ProducerFactory<String, String> producerFactory() {
+		Map<String, Object> configProps = new HashMap<>();
+		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		return new DefaultKafkaProducerFactory<>(configProps);
+	}
+	@Bean
+	public KafkaTemplate<String, String> kafkaTemplate() {
+		return new KafkaTemplate<>(producerFactory());
+	}
 }
